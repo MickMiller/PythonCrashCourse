3fa94c5f44e8b1556aee936419797373e9523e73
GMMil <GMMiller00@gmail.com>
2019-07-28 10:40:50 -0400

    useful updates

--- ch10.py
+++ ch10.py
@@ -1,27 +1,27 @@
 """ File: ch10.py
-   Additional methods for test_ch10
+   Additional methods for test_ch10.
 """
 
 
 def return_file(local_object):
-    """ Print entire file """
+    """ Print entire file. """
     file_contents = local_object.class_return_file(local_object.file_name)
     return file_contents
 
 
 def return_file_line(local_object, line_number):
-    """ Print file line by line """
+    """ Print file line by line. """
     line = local_object.class_return_line(line_number)
     return line
 
 
 def return_file_52(local_object):
-    """ Print entire file one line at a time """
+    """ Print first 52 chars in file. """
     file_contents = local_object.class_return_file(local_object.file_name)
     return file_contents[:52]
 
 
 def count_words(local_object):
-    """ See title """
+    """ See title. """
     num_words = local_object.class_count_words()
     return num_words
